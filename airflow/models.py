c77ac156dd18d35f9d7f097f402cd595c5f914f3
Sebastian Radloff <sebastian@slicelife.com>
2018-05-18 15:18:53 -0400

    SHR | Forces pickle protocol 2

--- airflow/models.py
+++ airflow/models.py
@@ -3564,7 +3564,7 @@ class DAG(BaseDag, LoggingMixin):
         d['is_picklable'] = True
         try:
             dttm = datetime.utcnow()
-            pickled = pickle.dumps(self)
+            pickled = pickle.dumps(self, protocol=2)
             d['pickle_len'] = len(pickled)
             d['pickling_duration'] = "{}".format(datetime.utcnow() - dttm)
         except Exception as e:
@@ -4100,7 +4100,7 @@ class XCom(Base, LoggingMixin):
             enable_pickling = configuration.getboolean('core', 'enable_xcom_pickling')
 
         if enable_pickling:
-            value = pickle.dumps(value)
+            value = pickle.dumps(value, protocol=2)
         else:
             try:
                 value = json.dumps(value).encode('UTF-8')
